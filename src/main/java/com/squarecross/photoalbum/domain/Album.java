20eb567152f2292d71a6420f20efcb00783d85a8
ulmu0426 <kcor24@naver.com>
2023-03-01 23:02:59 +0900

    Album Domain Class 구현

---
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -0,0 +1,47 @@
+package com.squarecross.photoalbum.domain;
+
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
+
+@Entity
+@Table(name = "album", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //가장 최근 id에 +1해서 다음 아이디 생성
+    @Column(name = "album_id", unique = true, nullable = false)
+    private Long albumId;
+
+    @Column(name = "album_name", unique = false, nullable = false)
+    private String albumName;
+
+    @Column(name = "created_at", unique = false, nullable = true)
+    @CreationTimestamp  //신규 앨범 생성 및 DB INSERT시 자동으로 현재 시간을 입력
+    private Date createdAt;
+
+    public Album(){};
+
+    public Long getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Long albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
