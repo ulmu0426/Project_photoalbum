f3fc1723e21debf8251be5013b8074f77785a79c
ulmu0426 <kcor24@naver.com>
2023-03-02 20:58:51 +0900

    AlbumService Class 구현, Test Case 작성 및 테스트

--- src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
-
+    //기본 CRUD기능은 구현되어있어서 제공되는 메서드를 호출하면 됨.
 }
---
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,31 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest     //스프링 컨테이너 내에 있는 빈을 DI로 가져와 사용할 수 있도록 @Autowired로 모든 빈 사용 가능
+@Transactional
+class AlbumServiceTest {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    AlbumService albumService;
+
+    @Test
+    void getAlbum() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(album);
+
+        Album resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
+        assertEquals("테스트", resAlbum.getAlbumName());
+    }
+}
